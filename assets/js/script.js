b3f7555ff4f85d609b7c074f8e4ec0bdfd767f4a
Flunder620 <jguzman12310@yahoo.com>
2023-08-26 13:03:03 -0500

    added comments in html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 var search = document.querySelector('.search-btn');
 var userFormEl = document.querySelector('.user-form');
+var cityNameEl = document.querySelector('#city-name')
 var searchedCitiesEl = document.querySelector('.searched-cities');
 
-var apiKey = "29606f93b69cb8597014cd175be1c24d";
-var queryURL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+// var apiKey = "29606f93b69cb8597014cd175be1c24d";
+// var queryURL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
 
 function formSubmitHandler() {
-    event.preventDefault()
-
+    event.preventDefault();
+    var apiKey = "29606f93b69cb8597014cd175be1c24d";
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+    var city = cityNameEl.value.trim()
+    
 };
 // fetch(queryURL)
 search.addEventListener('click', formSubmitHandler);
