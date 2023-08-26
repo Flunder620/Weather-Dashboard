1cc95703ebe4d58136957011fe498a7d7351fe6b
Flunder620 <jguzman12310@yahoo.com>
2023-08-26 12:39:41 -0500

    added apy key and url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
-var serach = document.querySelector('.user-form button')
+var search = document.querySelector('.search-btn');
+var userFormEl = document.querySelector('.user-form');
+var searchedCitiesEl = document.querySelector('.searched-cities');
 
-var apiKey= "29606f93b69cb8597014cd175be1c24d";
-var queryURL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+var apiKey = "29606f93b69cb8597014cd175be1c24d";
+var queryURL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
 
-fetch(queryURL)
+function formSubmitHandler() {
+    event.preventDefault()
+
+};
+// fetch(queryURL)
+search.addEventListener('click', formSubmitHandler);
